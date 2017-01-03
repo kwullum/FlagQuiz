94cc8b16050fb585d697c084ce9ce85feb019cc9
karolinawullum <kwullum@gmail.com>
2017-01-03 15:47:39 +0000

    Finally works. Don't mess up.

--- app/src/main/java/com/example/karolinawullum/quizapp/Home.java
+++ app/src/main/java/com/example/karolinawullum/quizapp/Home.java
@@ -5,40 +5,35 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 public class Home extends AppCompatActivity {
 
-    Button btnSignIn,btnSignUp;
+    //Button btnSignIn,btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Get The Reference Of Buttons
-        btnSignIn = (Button)findViewById(R.id.buttonSignIN);
-        btnSignUp = (Button)findViewById(R.id.buttonSignUP);
+        Button btnSignIn = (Button) findViewById(R.id.buttonSignIN);
+        Button btnSignUp = (Button) findViewById(R.id.buttonSignUP);
 
-    }
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSignIN = new Intent(Home.this, LoginActivity.class);
+                startActivity(intentSignIN);
+            }
+        });
 
-            public void onClick(View view) {
-
-                if(view.getId() == R.id.buttonSignIN) {
-                    Intent intentSignIN = new Intent(this, LoginActivity.class);
-                        startActivity(intentSignIN);
-                }
-                else if(view.getId() == R.id.buttonSignUP) {
-                    Intent intentSignUP = new Intent(this, Register.class);
-                        startActivity(intentSignUP);
-                }
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSignUP = new Intent(Home.this, Register.class);
+                startActivity(intentSignUP);
 
             }
-
+        });
     }
-
-
-
-
-
-
 }
